9335fb1758514a507a15d770c02937572cc0620f
uongtienduc <traanfhwng@gmail.com>
2022-03-15 20:28:18 +0700

    Lab05 update2

--- app/src/main/java/com/example/lab05/Detail.java
+++ app/src/main/java/com/example/lab05/Detail.java
@@ -3,10 +3,12 @@ package com.example.lab05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Detail extends AppCompatActivity {
-TextView ten, gia, chitiet;
+    TextView ten, gia, chitiet;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +16,14 @@ TextView ten, gia, chitiet;
 
         ten = findViewById(R.id.ten);
         ten.setText(getIntent().getStringExtra("name"));
+
+        gia = findViewById(R.id.gia);
+        gia.setText(getIntent().getStringExtra("price"));
+
+        chitiet = findViewById(R.id.chitiet);
+        chitiet.setText(getIntent().getStringExtra("about"));
+
+        img = findViewById(R.id.img);
+        img.setImageResource(getIntent().getIntExtra("img",0));
     }
 }
